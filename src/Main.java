ba89adc3e9214727f32380b0e77059d980c9c9e0
kopysovdmitry <kopysov.dmitry82@gmail.com>
2022-11-15 05:11:36 +0300

    Домашняя работа по массивам часть 1

--- src/Main.java
+++ src/Main.java
@@ -6,49 +6,55 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] numbers = new int [3];
-        int i =0;
-        while (i<numbers.length-1){
-            i=i+1;
+        int[] numbers = new int[3];
+        int i = 0;
+        while (i < numbers.length - 1) {
+            i = i + 1;
             numbers[i] = i;
-            System.out.print(numbers[i]+",");}
-            System.out.print(numbers.length);
+            System.out.print(numbers[i] + ",");
+        }
+        System.out.print(numbers.length);
         System.out.println(" ");
 
-        double[] num = {1.57,7.654,9.986};
-        int d=-1;
-        while (d < num.length-(num.length-1)){
-            d=d+1;
-            System.out.print(num[d]+",");}
-        System.out.print(num[num.length-1]);
+        double[] num = {1.57, 7.654, 9.986};
+        int d = -1;
+        while (d < num.length - (num.length - 1)) {
+            d = d + 1;
+            System.out.print(num[d] + ",");
+        }
+        System.out.print(num[num.length - 1]);
         System.out.println(" ");
 
-        int [] integers = {1,2,3,4,5,6,7,8,9,10};
-        for (int ii = 0; ii<integers.length-1; ii++) {
-            System.out.print(integers[ii]+",");}
-            System.out.println(integers[9]);
+        int[] integers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int ii = 0; ii < integers.length - 1; ii++) {
+            System.out.print(integers[ii] + ",");
+        }
+        System.out.println(integers[9]);
 
-        int[] numbers_1 = new int [3];
-        int i_1 =numbers_1.length-1;
-        for (i_1=numbers_1.length-1; i_1>0; i_1=i_1-1){
-            numbers_1[i_1]=i_1+1;
+        int[] numbers_1 = {1, 2, 3};
+        for (int i_1 = numbers_1.length - 1; i_1 >= 0; i_1 = i_1-1) {
+            System.out.print(numbers_1[i]);
+            if (i_1 != 0) {
+                System.out.println(", ");
+                System.out.println(" ");
+            }
+        }
 
-            System.out.print(numbers[i]+",");}
-        System.out.print(numbers_1[0]);
-        System.out.println(" ");
-        double[] num_1 = {1.57,7.654,9.986};
-        int d_1= num_1.length;
-        while (d_1>1){
-            d_1=d_1-1;
-            System.out.print(num_1[d_1]+",");}
+        double[] num_1 = {1.57, 7.654, 9.986};
+        int d_1 = num_1.length;
+        while (d_1 > 1) {
+            d_1 = d_1 - 1;
+            System.out.print(num_1[d_1] + ",");
+        }
         System.out.print(num_1[0]);
         System.out.println(" ");
 
-        int [] integers_1 = {1,2,3,4,5,6,7,8,9,10};
+        int[] integers_1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int ii = integers_1.length;
-        while (ii>1){
-            ii=ii-1;
-        System.out.print(integers_1[ii]+",");}
+        while (ii > 1) {
+            ii = ii - 1;
+            System.out.print(integers_1[ii] + ",");
+        }
         System.out.print(integers_1[0]);
 
     }
