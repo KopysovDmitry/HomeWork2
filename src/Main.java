fcb7f929de9be5c9a9e4724aa777fbecf685842e
kopysovdmitry <kopysov.dmitry82@gmail.com>
2022-11-15 12:19:29 +0300

    Домашняя работа по массивам часть 1

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //task 1//
+
         int[] numbers = new int[3];
         int i = 0;
         while (i < numbers.length - 1) {
@@ -16,6 +18,8 @@ public class Main {
         System.out.print(numbers.length);
         System.out.println(" ");
 
+        //task 2//
+
         double[] num = {1.57, 7.654, 9.986};
         int d = -1;
         while (d < num.length - (num.length - 1)) {
@@ -25,20 +29,27 @@ public class Main {
         System.out.print(num[num.length - 1]);
         System.out.println(" ");
 
+        //task 3//
+
         int[] integers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         for (int ii = 0; ii < integers.length - 1; ii++) {
             System.out.print(integers[ii] + ",");
         }
         System.out.println(integers[9]);
 
+        //task 4//
+
         int[] numbers_1 = {1, 2, 3};
-        for (int i_1 = numbers_1.length - 1; i_1 >= 0; i_1 = i_1-1) {
+        for (int i_1 = numbers_1.length - 1; i_1 >= 0; i_1 = i_1 - 1) {
             System.out.print(numbers_1[i]);
             if (i_1 != 0) {
-                System.out.println(", ");
-                System.out.println(" ");
+                System.out.print(", ");
             }
+            System.out.println(" ");
+
         }
+
+        //task 5//
 
         double[] num_1 = {1.57, 7.654, 9.986};
         int d_1 = num_1.length;
@@ -49,6 +60,8 @@ public class Main {
         System.out.print(num_1[0]);
         System.out.println(" ");
 
+        //task 6//
+
         int[] integers_1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int ii = integers_1.length;
         while (ii > 1) {
@@ -56,6 +69,18 @@ public class Main {
             System.out.print(integers_1[ii] + ",");
         }
         System.out.print(integers_1[0]);
+        System.out.println(" ");
+
+        //task 7//
+
+        int[] arr = {1, 2, 3};
+        for (int i1 = 0; i1 < arr.length; i1++) {
+            if (arr[i1] % 2 != 0) {
+                arr[i] += 1;
+            }
+        }
+            System.out.println(Arrays.toString(arr));
+            System.out.println(" ");
 
     }
 }
